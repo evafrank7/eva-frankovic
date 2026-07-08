6057328bfa84e1b130b8894a58bca1493c471834
Eva Frankovic <emfrankovic7@gmail.com>
2026-07-08 19:30:25 -0400

    Fixing import

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import AboutMore from '../components/AboutMore'
-import Paris from '../assets/images/Paris.JPG'
+import Paris from '../assets/images/paris.JPG'
 
 const AboutPage = () => {
   return (
