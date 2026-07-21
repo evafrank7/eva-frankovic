aeb42204ff264fa41222386eaa8305be0e73242f
Eva Frankovic <emfrankovic7@gmail.com>
2026-07-21 07:36:54 -0400

    Type fix

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row items-center gap-8">
                 <section className="flex-1 w-auto">
                     <h2 className="uppercase pb-2"> About Me </h2>
-                    <p className='pb-8'> I am a developer who primarly loves to work on the frontend, but can do some backend work as well. Outside of coding, you can find me getting a coffee, a cocktail, or hanging out with dog.</p>
+                    <p className='pb-8'> I am a developer who primarly loves to work on the frontend, but can do some backend work as well. Outside of coding, you can find me getting a coffee, a cocktail, or hanging out with my dog.</p>
                     <Link
                         to="/about"
                         className="uppercase underline underline-offset-4"
